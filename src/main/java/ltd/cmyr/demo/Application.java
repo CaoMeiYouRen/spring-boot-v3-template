78b9d6aac8586c71058cc6cfac53f15540353145
CaoMeiYouRen <996881204@qq.com>
2023-10-26 01:30:42 +0800

    fix: 移除 xml 配置；新增 日志配置

--- src/main/java/ltd/cmyr/demo/Application.java
+++ src/main/java/ltd/cmyr/demo/Application.java
@@ -1,6 +1,8 @@
 package ltd.cmyr.demo;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("访问地址：http://127.0.0.1:8080");
-		System.out.println("文档地址：http://127.0.0.1:8080/swagger-ui.html");
+		logger.info("访问地址：http://127.0.0.1:8080");
+		logger.info("文档地址：http://127.0.0.1:8080/swagger-ui.html");
 	}
 
 }
--- src/main/java/ltd/cmyr/demo/mapper/UserMapper.java
+++ src/main/java/ltd/cmyr/demo/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package ltd.cmyr.demo.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import ltd.cmyr.demo.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import ltd.cmyr.demo.entity.User;
  * @author caomeiyouren
  * @since 2023-10-08
  */
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 
 }
